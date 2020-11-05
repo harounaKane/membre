191743261e7af8b7d10562a40cadf8a6a44b316a
harounaKane <harouna.kane@hotmail.fr>
2020-11-05 10:34:07 +0100

    suppression et ajout de membre

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const data =
      {id: 2, prenom: "Pierre", age: 50},
      {id: 3, prenom: "Toto", age: 18}
 ]
+
 //middleware  
 app.use( (req, res, next) => {
      console.log("URL : " + req.url);
@@ -24,13 +25,36 @@ app.get('/membre/:id', (req, res) => {
      res.send("Bonsoir " + data[ (req.params.id - 1) ].prenom +" ")
 })
 app.get('/delete/:id', (req, res) => {
-     let index = req.params.id - 1;
-     data.splice(index, 1);
+     let index = getIndex( req.params.id );
+     if(!isNaN(index)){
+          data.splice(index, 1);
+     }
      res.redirect('/');
 })
 app.post('/add', (req, res) => {
-     console.log(req.body.prenom);
-     res.send(req.body.prenom);
+     if(req.body.prenom != ""){
+          let membre = {
+               id: createID(),
+               prenom: req.body.prenom,
+               age:  req.body.age
+          }
+          data.push(membre);
+     }
+     res.redirect('/');
 })
 
 app.listen(8000, () => console.log("started at port 8080"))
+
+
+function getIndex(id){
+     for (let i = 0; i < data.length; i++) {
+          if( id == data[i].id ){
+               return i;
+          }
+     }
+     return "Mauvais ID";
+}
+
+function createID(){
+     return data[data.length - 1].id + 1;
+}
